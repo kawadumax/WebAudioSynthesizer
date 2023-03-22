1f3480fea2fcb6d11243366704a277bf30a66edf
kawadumax <kazmax7@gmail.com>
2023-03-22 20:47:28 +0900

    音が鳴るようになった

--- src/Synth.tsx
+++ src/Synth.tsx
@@ -1,20 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 const Synth = () => {
+  // オーディオコンテキストの初期化
+  const [synthEnabled, setSynthEnabled] = useState(false);
+  const [oscillator, setOscillator] = useState<OscillatorNode | null>(null);
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+
   useEffect(() => {
-    // オーディオコンテキストの初期化
-    const audioContext = new (window.AudioContext || window.AudioContext)();
-
     // ここにオーディオ関連の処理を記述します
-    const gainNode = audioContext.createGain();
-    const oscillator = audioContext.createOscillator();
-
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-
-    oscillator.frequency.setValueAtTime(440, audioContext.currentTime); // A4
-    oscillator.type = "sine";
-    // oscillator.start();
+    const audioContext = new window.AudioContext();
+    setAudioContext(audioContext);
 
     // コンポーネントのクリーンアップ時にオーディオコンテキストを閉じます
     return () => {
@@ -22,19 +17,41 @@ const Synth = () => {
     };
   }, []); // 空の依存配列を指定して、このエフェクトをコンポーネントのマウント時にのみ実行します
 
-  const [synthEnabled, setSynthEnabled] = useState(false);
-
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (audioContext === null) {
+      return;
+    }
+
     setSynthEnabled(event.target.checked);
+
     if (event.target.checked) {
       // シンセサイザをオンにする処理
+      const gainNode = audioContext.createGain();
+      const oscillator = audioContext.createOscillator();
+
+      oscillator.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+
+      oscillator.frequency.setValueAtTime(440, audioContext.currentTime); // A4
+      oscillator.type = "sine";
+
+      oscillator.start();
+
+      oscillator.onended = () => {
+        oscillator.disconnect(gainNode);
+        gainNode.disconnect(audioContext.destination);
+      };
+
+      setOscillator(oscillator);
     } else {
-      // シンセサイザをオフにする処理
+      if (oscillator) {
+        oscillator.stop();
+      }
     }
   };
 
   return (
-  <div className="Synth">
+    <div className="Synth">
       <label htmlFor="synthToggle">Synth On/Off</label>
       <input
         type="checkbox"
@@ -42,7 +59,7 @@ const Synth = () => {
         checked={synthEnabled}
         onChange={handleCheckboxChange}
       />
-  </div>
+    </div>
   );
 };
 
