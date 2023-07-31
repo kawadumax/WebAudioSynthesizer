d87b14247f067cb810b5b30b9ef31d047ad84b74
kawadumax <kazmax7@gmail.com>
2023-07-31 10:40:54 +0900

    [wip]オーディオコンテクストのテストを追加中

---
+++ tests/__mocks__/AudioContext.mock.ts
@@ -0,0 +1,14 @@
+// AudioContextのモックを作成
+window.AudioContext = jest.fn(() => ({
+  createOscillator: jest.fn(() => ({
+    start: jest.fn(),
+    connect: jest.fn(),
+    frequency: { value: 0 },
+  })),
+  createGain: jest.fn(() => ({
+    gain: { value: 0, setValueAtTime: jest.fn() },
+    connect: jest.fn(),
+  })),
+  close: jest.fn(),
+})) as any;
+  
--- tests/circuits/AudioContextCircuit.test.tsx
+++ tests/circuits/AudioContextCircuit.test.tsx
@@ -1,9 +1,6 @@
-import React, { useContext, useState, createContext } from "react";
-import { Tone } from "@components/circuits/TypeCircuit";
-import { useAudioContextCircuit } from "@components/circuits/AudioContextCircuit";
+import { renderHook, act } from "@testing-library/react";
+import AudioContextCircuit, {
+  useAudioContextCircuit,
+} from "@components/circuits/AudioContextCircuit";
 
-describe("AudioContextCircuit", () => {
-    it("2 +2=4", () => {
-        expect(2 + 2).toBe(4);
-    });
-});
+describe("AudioContextCircuit", () => {});
