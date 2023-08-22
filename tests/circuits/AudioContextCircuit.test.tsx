7bb9adf75fcf4e80158db753b333dada74122a45
kawadumax <kazmax7@gmail.com>
2023-08-22 10:55:06 +0900

    STOPのテストケース追加

--- tests/circuits/AudioContextCircuit.test.tsx
+++ tests/circuits/AudioContextCircuit.test.tsx
@@ -4,25 +4,27 @@ import { soundStateReducer } from "@circuits/AudioContextCircuit/SoundStateReduc
 import { SoundState, SoundStateAction, Tone } from "@circuits/TypeCircuit";
 import React, { Dispatch, useReducer } from "react";
 
-
 describe("AudioContextCircuit", () => {
-  const testTone = { name: "A", freq: 440 }
+  const testTone = { name: "A4", freq: 440 };
 
   describe("useSoundStatesEffect", () => {
     let audioContextMock: Partial<AudioContext>;
     let gainNodeMock: Partial<GainNode>;
-    let soundStatesMock: Partial<SoundState[]>
-    let dispatchMock: Partial<React.Dispatch<SoundStateAction>>
+    let soundStatesMock: Partial<SoundState[]>;
+    let dispatchMock: Partial<React.Dispatch<SoundStateAction>>;
     let hookResult: ReturnType<typeof runRenderHook>;
     function runRenderHook() {
-      return renderHook((soundState) => useSoundStatesEffect(
-        audioContextMock as AudioContext,
-        gainNodeMock as GainNode,
-        soundState as SoundState[],
-        dispatchMock as Dispatch<SoundStateAction>
-      ), { initialProps: soundStatesMock });
-    };
-
+      return renderHook(
+        (soundState) =>
+          useSoundStatesEffect(
+            audioContextMock as AudioContext,
+            gainNodeMock as GainNode,
+            soundState as SoundState[],
+            dispatchMock as Dispatch<SoundStateAction>
+          ),
+        { initialProps: soundStatesMock }
+      );
+    }
 
     beforeEach(() => {
       // AudioContextとGainNodeのモックをセットアップ
@@ -35,9 +37,9 @@ describe("AudioContextCircuit", () => {
             start: jest.fn(),
             stop: jest.fn(),
             frequency: {
-              value: undefined
+              value: undefined,
             } as Partial<AudioParam>,
-          } as Partial<OscillatorNode>
+          } as Partial<OscillatorNode>;
         }),
       };
       gainNodeMock = {
@@ -54,14 +56,14 @@ describe("AudioContextCircuit", () => {
         const testSoundState: SoundState = {
           tone: testTone,
           isStarted: true,
-          isEnded: false
+          isEnded: false,
         };
-        soundStatesMock = [testSoundState]
+        soundStatesMock = [testSoundState];
         hookResult.rerender(soundStatesMock);
       });
       expect(audioContextMock.createOscillator).toBeCalledTimes(1);
       expect(soundStatesMock[0]?.oscillator).toBeTruthy;
-    })
+    });
 
     it("オシレータが追加されたあと、isEndedをtrueにするとオシレータが削除される", () => {
       expect(audioContextMock.createOscillator).toBeCalledTimes(0);
@@ -69,9 +71,9 @@ describe("AudioContextCircuit", () => {
         const testSoundState: SoundState = {
           tone: testTone,
           isStarted: true,
-          isEnded: false
+          isEnded: false,
         };
-        soundStatesMock = [testSoundState]
+        soundStatesMock = [testSoundState];
         hookResult.rerender(soundStatesMock);
       });
       expect(audioContextMock.createOscillator).toBeCalledTimes(1);
@@ -79,7 +81,7 @@ describe("AudioContextCircuit", () => {
       act(() => {
         if (soundStatesMock[0]) {
           soundStatesMock[0].isEnded = true;
-        };
+        }
         soundStatesMock = [...soundStatesMock];
         hookResult.rerender(soundStatesMock);
       });
@@ -87,8 +89,8 @@ describe("AudioContextCircuit", () => {
       expect(dispatchMock).toBeCalledTimes(1);
       expect(soundStatesMock).toHaveLength(1);
       expect(soundStatesMock[0]?.oscillator).toBeFalsy;
-    })
-  })
+    });
+  });
 
   describe("useSoundStatesReducer", () => {
     describe("dispatch経由でsoundReducerを実行してsoundStatesがどう変わるか", () => {
@@ -103,7 +105,7 @@ describe("AudioContextCircuit", () => {
         });
         // console.log(hookResult.result.current);
         expect(hookResult.result.current[0]).toHaveLength(1);
-      })
+      });
 
       it("START_SOME", () => {
         const hookResult = renderHook(() => useReducer(soundStateReducer, []));
@@ -112,21 +114,36 @@ describe("AudioContextCircuit", () => {
         expect(soundStates).toHaveLength(0);
         const tones = [
           { freq: 440, name: "A4" },
-          { freq: 493.8833012561241, name: "B4" }
-        ]
+          { freq: 493.8833012561241, name: "B4" },
+        ];
         act(() => {
           dispatch({ type: "START_SOME", payload: tones });
           hookResult.rerender();
         });
         // console.log(hookResult.result.current);
         expect(hookResult.result.current[0]).toHaveLength(2);
-      })
-      it.todo("STOP")
-      it.todo("STOP_EXCEPT")
-      it.todo("STOP_EXCEPTS")
-      it.todo("STOP_ALL")
-      it.todo("CLEAR")
-    })
-  })
-
+      });
+      it("STOP", () => {
+        const hookResult = renderHook(() => useReducer(soundStateReducer, []));
+        let [soundStates, dispatch] = hookResult.result.current;
+        expect(soundStates).toHaveLength(0);
+        act(() => {
+          dispatch({ type: "START", payload: testTone });
+          hookResult.rerender();
+        });
+        expect(hookResult.result.current[0]).toHaveLength(1);
+        act(() => {
+          dispatch({ type: "STOP", payload: testTone });
+          hookResult.rerender();
+        });
+        expect(hookResult.result.current[0]).toHaveLength(1);
+        // console.log(hookResult.result.current[0]);
+        expect(hookResult.result.current[0][0].isEnded).toBeTruthy;
+      });
+      it.todo("STOP_EXCEPT");
+      it.todo("STOP_EXCEPTS");
+      it.todo("STOP_ALL");
+      it.todo("CLEAR");
+    });
+  });
 });
