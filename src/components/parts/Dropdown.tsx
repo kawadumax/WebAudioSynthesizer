c40e2b8d200f77481d01f65210b28f88e6b66071
kawadumax <kazmax7@gmail.com>
2023-10-28 13:48:40 +0900

    WaveFormSelectorの表示機構作成

--- src/components/parts/Dropdown.tsx
+++ src/components/parts/Dropdown.tsx
@@ -1,15 +1,28 @@
 // src/components/Dropdown.tsx
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, LiHTMLAttributes } from 'react';
 import '@styles/Dropdown.scss';
 
 interface Props {
     options: string[];
+    onChange: (value: string) => void;
 }
 
-const Dropdown: React.FC<Props> = ({ options }) => {
+const Dropdown: React.FC<Props> = ({
+    options,
+    onChange
+}) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleDropdown = () => setIsOpen(!isOpen);
+    const [value, setValue] = useState(options[0]);
+
+    useEffect(() => {
+        onChange(value);
+    }, [value]);
+
+    const selectOptionHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+        setValue(e.currentTarget.getAttribute("data-value") as string);
+    }
 
     return (
         <div className="dropdown">
@@ -19,7 +32,7 @@ const Dropdown: React.FC<Props> = ({ options }) => {
             {isOpen && (
                 <ul className="dropdown-menu">
                     {options.map((option, index) => (
-                        <li key={index} className="dropdown-item">
+                        <li key={index} data-value={option} className="dropdown-item" onClick={selectOptionHandler}>
                             {option}
                         </li>
                     ))}
