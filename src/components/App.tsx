da0dc1d3acb69d8d7c0a48b74f122ab0803fc0b7
kawadumax <kazmax7@gmail.com>
2023-11-10 14:01:05 +0900

    wip: forcing landscape mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Main from "@components/Main";
 import Header from "@components/Header";
 import "@styles/App.scss";
 
 function App() {
+  useEffect(() => {
+    const orientation: ScreenOrientation = window.screen.orientation;
+    (orientation as any).lock("landscape");
+
+    return () => {
+      orientation.unlock();
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header />
