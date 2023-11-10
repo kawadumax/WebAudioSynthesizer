5a17ec4999b7f78943edfcc9472f4898e7c1bc41
kawadumax <kazmax7@gmail.com>
2023-11-10 16:16:47 +0900

    wip:

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,15 +4,6 @@ import Header from "@components/Header";
 import "@styles/App.scss";
 
 function App() {
-  useEffect(() => {
-    const orientation: ScreenOrientation = window.screen.orientation;
-    (orientation as any).lock("landscape");
-
-    return () => {
-      orientation.unlock();
-    };
-  }, []);
-
   return (
     <div className="App">
       <Header />
